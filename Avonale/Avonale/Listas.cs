bc48a8bade611d5622bf2996b11657962c4f245e
Ian Santana <ianbs1@outlook.com>
2019-05-17 12:07:56 -0300

    Add files via upload

---
+++ Avonale/Avonale/Listas.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avonale
+{
+    class Listas
+    {
+        // Classe com os métodos da questão 02.
+
+        private int _quantidade;
+        public double Soma { get; private set; }
+
+        public Listas()
+        {
+
+        }
+
+        public Listas(int quantidade, double soma)
+        {
+            _quantidade = quantidade;
+            Soma = soma;
+        }
+
+
+
+
+        public int Quantidade
+        {
+            get { return _quantidade; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _quantidade = value;
+                }
+            }
+        }
+
+
+        public Double CalcMedia(int[] vetor, int quantidade, double soma)
+        {
+
+            if (quantidade > 0)
+            {
+                soma = soma + vetor[quantidade - 1];
+                return CalcMedia(vetor, quantidade - 1, soma);
+
+            }
+            else return soma / _quantidade;
+
+        }
+
+        public List<int> MaiorQMedia(List<int> num, double media)
+        {
+            Quantidade = num.Count;
+            if (num == null || Quantidade <= 0)
+            {
+                return null;
+            }
+            else if (num[num.Count - 1] > media)
+            {
+                Console.Write(num[num.Count - 1] + " ");
+
+
+
+            }
+            num.RemoveAt(num.Count - 1);
+            return MaiorQMedia(num, media);
+
+
+
+
+        }
+
+
+
+        public List<char> Inverte(List<char> list)
+        {
+
+
+            Quantidade = list.Count;
+            if (list == null || Quantidade <= 0)
+            {
+                return null;
+            }
+
+            Console.Write(list[list.Count - 1] + " ");
+            list.RemoveAt(list.Count - 1);
+            return Inverte(list);
+        }
+
+    }
+}
---
+++ Avonale/Avonale/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Avonale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double resultado;
+            int[] vetor = new int[7] { -10, 4, 8, 20, 6, 0, -7 };
+
+
+            Listas m = new Listas(vetor.Length, 0);
+
+
+
+            resultado = m.CalcMedia(vetor, m.Quantidade, m.Soma); // Calcula a media 
+
+            Console.WriteLine("A média é: " + resultado);
+
+            List<int> numeros = new List<int>();
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                numeros.Add(vetor[i]);
+            }
+            Console.Write("OS números maiores que a média são : ");
+            m.MaiorQMedia(numeros, resultado); // Calcula os números maiores que a média
+            Console.WriteLine("");
+
+
+            List<char> list = new List<char>();
+            list.Add('D');
+            list.Add(':');
+            list.Add('a');
+            list.Add('t');
+            list.Add('a');
+            list.Add('r');
+            list.Add('t');
+            list.Add('n');
+            list.Add('o');
+            list.Add('c');
+            list.Add('e');
+            list.Add('M');
+
+            Console.Write("Lista original: ");
+            list.ForEach(Console.Write);
+            Console.WriteLine("");
+
+
+            Console.Write("A lista invertida é: ");
+            m.Inverte(list); // Retorna a lista invertida
+            Console.WriteLine("");
+
+
+        }
+
+
+
+
+
+
+    }
+}
